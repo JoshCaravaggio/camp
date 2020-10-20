28ca6d7d1a904470b4b261259506732e6eef2e4f
JoshCaravaggio <jdcaravaggio@gmail.com>
2020-10-20 19:33:44 -0400

    Added public files and fixed CSS

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  get '/', :to => redirect('/index.html')
+  get '/home', :to => redirect('/index.html')
+  get '/about', :to => redirect('/about.html')
+  get '/off-ice-sessions', :to => redirect('/off-ice-sessions.html')
+  get '/on-ice-sessions', :to => redirect('/on-ice-sessions.html')
+
+end
